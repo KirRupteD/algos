6b42c838bc47f2893dc835597de0a5061fbcd631
KirRupteD <randy_cornelius@live.com>
2017-02-14 20:10:17 -0400

    search function implemented

--- search.cpp
+++ search.cpp
@@ -67,3 +67,13 @@ int main()
 
     return 0;
 }
+int linearSearch(auto Data, auto key)
+{
+for(int i = 0; i < Data.size(); i ++)
+{
+if (Data[i] == key)//we found it
+{
+return i;//return its location
+}
+}//end for
+return -1;}//element not found
